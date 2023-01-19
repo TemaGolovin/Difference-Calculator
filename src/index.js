6e80a2147f41a2490b6df41d8a5c4ea4600fb5ee
TemaGolovin <golart1996@mail.ru>
2023-01-19 16:24:03 +0700

    add undex.js

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import _ from "lodash";
+import fs from "fs";
+import process from "process";
+import path from "path";
+
+const getfilePath = (filePath) => path.resolve(process.cwd(), filePath);
+
+const readFile = (path) => fs.readFileSync(path);
+
+const parsesFile = (file) => JSON.parse(file);
+
+const gendiff = (filePath1, filePath2) => {
+  const file1 = getfilePath(filePath1);
+  const file2 = getfilePath(filePath2);
+
+  const readedFile1 = readFile(file1);
+  const readedFile2 = readFile(file2);
+
+  const obj1 = parsesFile(readedFile1);
+  const obj2 = parsesFile(readedFile2);
+
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  const uniqKeys = [...new Set(keys1.concat(keys2))];
+  const sortUniqKeys = uniqKeys.slice().sort();
+  let result = "";
+  for (const key of sortUniqKeys) {
+    if (obj1[key] === obj2[key]) {
+      result = `${result}   ${key}: ${obj1[key]}\n`;
+    } else if (
+      _.has(obj1, key) &&
+      _.has(obj2, key) &&
+      obj1[key] !== obj2[key]
+    ) {
+      result = `${result} - ${key}: ${obj1[key]}\n + ${key}: ${obj2[key]}\n`;
+    } else if (_.has(obj1, key)) {
+      result = `${result} - ${key}: ${obj1[key]}\n`;
+    } else if (_.has(obj2, key)) {
+      result = `${result} + ${key}: ${obj2[key]}\n`;
+    }
+  }
+
+  return `{\n${result}}`;
+};
+
+export default gendiff;
