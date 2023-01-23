c2a8ea923d6ae48a60471867e24ae5666afed597
TemaGolovin <golart1996@mail.ru>
2023-01-23 17:29:35 +0700

    update

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,24 +1,19 @@
-import fs from "fs";
-import path from "path";
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { test, expect } from '@jest/globals';
 
-import { fileURLToPath } from "url";
-import { dirname } from "path";
+import { fileURLToPath } from 'url';
 
-import genDiff from "../src/index.js";
+import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) =>
-  path.join(__dirname, "..", "__fixtures__", filename);
-const readFile = (filename) =>
-  fs.readFileSync(getFixturePath(filename), "utf-8");
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test("gendiff flat files", () => {
+test('gendiff flat files', () => {
   expect(
-    genDiff(
-      getFixturePath("file1.json").trim(),
-      getFixturePath("file2.json").trim()
-    )
-  ).toEqual(readFile("expected.txt").trim());
+    genDiff(getFixturePath('file1.json'), getFixturePath('file2.json')),
+  ).toEqual(readFile('expected.txt'));
 });
