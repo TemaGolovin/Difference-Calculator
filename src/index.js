0d471eb99650d0c3f0e980880bed042c8d69ebe7
TemaGolovin <golart1996@mail.ru>
2023-01-24 14:15:50 +0700

    add parses

--- src/index.js
+++ src/index.js
@@ -2,22 +2,24 @@ import _ from "lodash";
 import fs from "fs";
 import process from "process";
 import path from "path";
+import parses from "./parses.js";
 
 const getfilePath = (filePath) => path.resolve(process.cwd(), filePath);
 
 const readFile = (pathToFile) => fs.readFileSync(pathToFile, "utf-8");
 
-const parsesFile = (file) => JSON.parse(file);
-
 const genDiff = (filePath1, filePath2) => {
   const file1 = getfilePath(filePath1);
   const file2 = getfilePath(filePath2);
 
+  const format1 = path.extname(file1);
+  const format2 = path.extname(file2);
+
   const readedFile1 = readFile(file1);
   const readedFile2 = readFile(file2);
 
-  const obj1 = parsesFile(readedFile1);
-  const obj2 = parsesFile(readedFile2);
+  const obj1 = parses(readedFile1, format1);
+  const obj2 = parses(readedFile2, format2);
 
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
