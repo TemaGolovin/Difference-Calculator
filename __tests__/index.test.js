9f33dc1f8b5df1d26181cd4c519a9f510a7dbbf4
TemaGolovin <golart1996@mail.ru>
2023-01-24 14:13:42 +0700

    add YAML format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,19 +1,27 @@
-import fs from 'fs';
-import path, { dirname } from 'path';
-import { test, expect } from '@jest/globals';
+import fs from "fs";
+import path, { dirname } from "path";
+import { test, expect } from "@jest/globals";
 
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 
-import genDiff from '../src/index.js';
+import genDiff from "../src/index.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const getFixturePath = (filename) =>
+  path.join(__dirname, "..", "__fixtures__", filename);
+const readFile = (filename) =>
+  fs.readFileSync(getFixturePath(filename), "utf-8");
 
-test('gendiff flat files', () => {
+test("gendiff flat files json-json", () => {
   expect(
-    genDiff(getFixturePath('file1.json'), getFixturePath('file2.json')),
-  ).toEqual(readFile('expected.txt'));
+    genDiff(getFixturePath("file1.json"), getFixturePath("file2.json"))
+  ).toEqual(readFile("expected.txt"));
+});
+
+test("gendiff flat files yaml-yaml", () => {
+  expect(
+    genDiff(getFixturePath("file3.yaml"), getFixturePath("file4.yaml"))
+  ).toEqual(readFile("expected.txt"));
 });
