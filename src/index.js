f30f05cab1c99c9645b5749c3957a0ec2296c765
TemaGolovin <golart1996@mail.ru>
2023-01-30 19:42:35 +0700

    added comparison of nested objects

--- src/index.js
+++ src/index.js
@@ -3,44 +3,71 @@ import fs from 'fs';
 import process from 'process';
 import path from 'path';
 import parses from './parses.js';
+import getFormat from './formatters/index.js';
+
+const findDiff = (obj1, obj2) => {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  const allKeys = _.sortBy(_.uniq([...keys1, ...keys2]));
+  const diffStructure = allKeys.map((key) => {
+    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+      return {
+        name: key,
+        value: findDiff(obj1[key], obj2[key]),
+        type: 'nested',
+      };
+    }
+    if (!_.has(obj2, key)) {
+      return {
+        name: key,
+        value: obj1[key],
+        type: 'deleted',
+      };
+    }
+    if (!_.has(obj1, key)) {
+      return {
+        name: key,
+        value: obj2[key],
+        type: 'added',
+      };
+    }
+    if (_.has(obj1, key) && _.has(obj2, key) && obj1[key] !== obj2[key]) {
+      return {
+        name: key,
+        value1: obj1[key],
+        value2: obj2[key],
+        type: 'changed',
+      };
+    }
+    return {
+      name: key,
+      value: obj1[key],
+      type: 'unchanged',
+    };
+  });
+  return diffStructure;
+};
 
 const getfilePath = (filePath) => path.resolve(process.cwd(), filePath);
 
-const readFile = (pathToFile) => fs.readFileSync(pathToFile, 'utf-8');
+const readFile = (pathToFile) => fs.readFileSync(getfilePath(pathToFile), 'utf-8');
 
-const genDiff = (filePath1, filePath2) => {
-  const file1 = getfilePath(filePath1);
-  const file2 = getfilePath(filePath2);
+const extname = (filePath) => path.extname(getfilePath(filePath));
 
-  const format1 = path.extname(file1);
-  const format2 = path.extname(file2);
+const genDiff = (filePath1, filePath2, format = 'stylish') => {
+  const format1 = extname(filePath1);
+  const format2 = extname(filePath2);
 
-  const readedFile1 = readFile(file1);
-  const readedFile2 = readFile(file2);
+  const readedFile1 = readFile(filePath1);
+  const readedFile2 = readFile(filePath2);
 
   const obj1 = parses(readedFile1, format1);
   const obj2 = parses(readedFile2, format2);
 
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
+  const diff = findDiff(obj1, obj2);
 
-  const uniqKeys = [...new Set(keys1.concat(keys2))];
-  const sortUniqKeys = uniqKeys.slice().sort();
-  let result = '';
-  /* eslint-disable-next-line */
-  for (const key of sortUniqKeys) {
-    if (_.has(obj1, key) && _.has(obj2, key) && obj1[key] !== obj2[key]) {
-      result = `${result} - ${key}: ${obj1[key]}\n + ${key}: ${obj2[key]}\n`;
-    } else if (obj1[key] === obj2[key]) {
-      result = `${result}   ${key}: ${obj1[key]}\n`;
-    } else if (_.has(obj1, key)) {
-      result = `${result} - ${key}: ${obj1[key]}\n`;
-    } else if (_.has(obj2, key)) {
-      result = `${result} + ${key}: ${obj2[key]}\n`;
-    }
-  }
-
-  return `{\n${result}}`;
+  return getFormat(diff, format);
 };
 
 export default genDiff;
