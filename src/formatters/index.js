5d6be25053d8080e9b46f4e4524ae749f772c73a
TemaGolovin <golart1996@mail.ru>
2023-01-31 16:25:47 +0700

    add plain format

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,12 @@
-import stylish from './stylish.js';
+import stylish from "./stylish.js";
+import plain from "./plain.js";
 
 const getFormat = (data, format) => {
   switch (format) {
-    case 'stylish':
+    case "stylish":
       return stylish(data);
+    case "plain":
+      return plain(data);
     default:
       throw new Error(`Unknown format: ${format}`);
   }
