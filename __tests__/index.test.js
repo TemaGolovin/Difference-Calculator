1f4c672a46988b7bac89d2f9b8feb1cdcd8b4af2
TemaGolovin <golart1996@mail.ru>
2023-01-31 16:34:41 +0700

    fix eslint

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,33 +1,31 @@
-import fs from "fs";
-import path, { dirname } from "path";
-import { test, expect } from "@jest/globals";
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { test, expect } from '@jest/globals';
 
-import { fileURLToPath } from "url";
+import { fileURLToPath } from 'url';
 
-import genDiff from "../src/index.js";
+import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) =>
-  path.join(__dirname, "..", "__fixtures__", filename);
-const readFile = (filename) =>
-  fs.readFileSync(getFixturePath(filename), "utf-8");
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test("gendiff nested objects json-json in stylish format", () => {
+test('gendiff nested objects json-json in stylish format', () => {
   expect(
-    genDiff(getFixturePath("file1.json"), getFixturePath("file2.json"))
-  ).toEqual(readFile("expected.txt"));
+    genDiff(getFixturePath('file1.json'), getFixturePath('file2.json')),
+  ).toEqual(readFile('expected.txt'));
 });
 
-test("gendiff nested objects yaml-yaml in stylish format", () => {
+test('gendiff nested objects yaml-yaml in stylish format', () => {
   expect(
-    genDiff(getFixturePath("file1.yaml"), getFixturePath("file2.yaml"))
-  ).toEqual(readFile("expected.txt"));
+    genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml')),
+  ).toEqual(readFile('expected.txt'));
 });
 
-test("gendiff nested objects in plain format", () => {
+test('gendiff nested objects in plain format', () => {
   expect(
-    genDiff(getFixturePath("file1.json"), getFixturePath("file2.json"), "plain")
-  ).toEqual(readFile("expectedPlainFormat.txt"));
+    genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain'),
+  ).toEqual(readFile('expectedPlainFormat.txt'));
 });
