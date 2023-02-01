9b5dd799cb25a95fc08f9b9a215d3ee9d9fd7584
TemaGolovin <golart1996@mail.ru>
2023-02-01 13:11:53 +0700

    add JSON format

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,11 +2,14 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 
 const getFormat = (data, format) => {
-  switch (format) {
+  const lowerCaseFormat = format.toLowerCase();
+  switch (lowerCaseFormat) {
     case 'stylish':
       return stylish(data);
     case 'plain':
       return plain(data);
+    case 'json':
+      return JSON.stringify(data);
     default:
       throw new Error(`Unknown format: ${format}`);
   }
